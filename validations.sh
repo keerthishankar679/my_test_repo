ae66b50046bad7acdfe2df261753e1927b069d42
Ubuntu <ubuntu@ip-172-31-39-166.us-east-2.compute.internal>
2024-01-02 06:30:08 +0000

    fi

---
+++ validations.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+ 
+# change with <+steps.List_Files_Changed.output.outputVariables.FILES>
+sample=$(git diff --name-only origin/main...HEAD)
+
+
+echo "***************changed_files************"
+for file in $sample; do
+    echo $file
+done    
+echo "****************************************"
+echo ""
+# Arrays to collect validation flag
+declare -a flag
+
+i=1
+
+for file in $sample; do
+    echo "***************************************"
+    echo "${i}. $file "
+    let i=i+1    
+    # Current dir of change file
+    directory=$(dirname $file)
+    # templateId=$(yq -r '.template.templateId' "$file")
+ 
+    # Step to validate the existence of README.md file in the directory
+    if [ -f "${directory}/README.md" ]; then  
+        flag[0]=1
+    else
+        :
+        #echo "Error: ${directory}/README.md file not found. Please include a README.md file in the directory before merging the pull request."    
+    fi
+    # Step to validate the existence of templateId.stable in the directory
+    file_name=$(basename $file)
+ 
+    if echo "$file_name"| grep -q ".stable" ; then
+ 
+        templateId=$(echo "$file_name"|grep -i ".stable"| cut -d "." -f 1)
+        stable_templateId=$(echo "$file_name"|grep -i ".stable"| cut -d "." -f 1)
+
+        if [ ! -f "${directory}/${templateId}.stable" ]; then
+            echo "$templateId"
+            #echo "Error: ${directory}/${templateId}.stable file not found. Please associate the file with the template before merging the pull request."
+            flag[1]=1
+        fi
+ 
+    else
+        templateId=""
+    fi
+
+    if [[ $file != *.yaml ]] || [[ $file == .harness/* ]]; then
+        continue
+    fi
+
+    templateId=`yq -r '.template.identifier' "$file"`
+    templateVersion=`yq -r '.template.versionLabel' "$file"`
+    templateType=$(yq -r '.template.type' "$file")
+    stableFile="${directory}/${templateId}.stable"
+    stableVersion=`cat $stableFile`
+    top_level_directory=$(dirname $file| grep -iE "stage|step|pipeline"| cut -d '/' -f 1 )
+
+    echo "templateId : $templateId"
+    echo "templateVersion : $templateVersion"
+    echo "stableVersion : $stableVersion"
+    echo "templateType : $templateType"
+    echo "top_level_directory : $top_level_directory"
+
+    if [[ "$file_name" == "${templateId}_${stableVersion}.yaml" ]]; then
+	    echo "stable version is pointing to a valid version : $file_name"
+		flag[2]=1
+    
+    fi	
+
+	if ! echo "$file_name" | grep -q "${stable_templateId}"; then
+	    flag[3]=1
+	    echo "Error: \"${file_name}\" is not containing the matching identifier \"${stable_templateId}\""  
+	fi
+
+    if [ "$top_level_directory" != "${templateType,,}s" ]; then
+        flag[4]=1
+    fi    
+
+done
+
+echo ""
+echo "*****************Final result**************"
+echo ""
+if [[ ${flag[0]} -gt 0 ]]; then
+    echo "README.md found!"	
+else	
+	echo "README.md not found" 
+fi
+if [[ ${flag[1]} -gt 0 ]]; then
+    echo "Stable not found!"
+else	
+	echo "Stable found" 
+fi	
+
+if [[ ${flag[2]} -gt 0 ]]; then
+
+  echo "The stable version is pointing to a valid version"
+
+else
+
+  echo "The stable version is not pointing to a valid version"
+  
+fi
+if [[ ${flag[3]} -gt 0 ]]; then
+    echo "Error: file is not containing the matching identifier \"${stable_templateId}\""
+else
+    echo "Idenfier is matching"
+fi
+if [[ ${flag[4]} -gt 0 ]]; then
+    echo "Error: Template type is not matching the top level directory"
+else
+    echo "All templates in top level directory is matching its type"    
+fi    
